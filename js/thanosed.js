07d47a63b8a6ee8a7db973a142ac5d56b248947b
Jessica <42528319+seaweeddol@users.noreply.github.com>
2019-01-07 08:56:38 -0800

    Update thanosed.js
    added more comments

--- js/thanosed.js
+++ js/thanosed.js
@@ -5,6 +5,7 @@ var audio = new Audio('sounds/Finger-snap.mp3');
 // initialize board
 init();
 
+// populate names array and create the board
 function init(){
   names = ["Iron Man", "Vision", "Captain America", "Thor", "Black Widow", "Pepper Pots", "War Machine", "Heimdall", "Loki", "The Hulk", "Bucky Barnes", "Falcon", "Hawkeye", "Drax", "Black Panther", "Rocket Raccoon", "Dr. Strange", "Groot", "Nebula", "Spiderman", "Scarlet Witch", "Starlord", "Ant-Man", "Gamora", "Okoye", "Mantis", "The Wasp"];
   createTiles(names.name);
@@ -12,6 +13,7 @@ function init(){
 
 // create tiles from names in array
 function createTiles(){
+  // loop through each item in the array and create a tile within the tileContainer div
   names.forEach(function(name){
     $(".tileContainer").fadeIn(500, function(){
       $(".tileContainer").append("<span class='tile'>" + name + "</span>");
@@ -22,13 +24,17 @@ function createTiles(){
 // remove approximately half of the tiles
 function thanosed(){
   names.forEach(function(name){
+    // creates a random number between 1-100
     var randomNum = Math.floor((Math.random() * 100) + 1);
+    // if random number is greater than 50, push the name into a new array
     if (randomNum > 50) {
       survivors.push(name);
     }
     console.log(name + " " + randomNum);
   });
+  // update names array with survivors array
   names = survivors;
+  // clear survivors array
   survivors = [];
   clearTiles();
   createTiles();
@@ -36,17 +42,18 @@ function thanosed(){
 
 // clear all tiles
 function clearTiles(){
+  // fadeout tileContainer div, and then set to inital display property
   $('.tileContainer').fadeOut(500, function() {
      $(this).empty().show();
   });
 };
 
-// empty array
+// empty names array
 function clearArray(){
   names = [];
 };
 
-// clear board, reset to original tiles, and recreate board
+// clear tiles and array, and initialize original tiles
 function reset(){
   clearTiles();
   clearArray();
@@ -55,17 +62,23 @@ function reset(){
 
 // create new tile based on user input
 function addTile(){
+  // create variable with useInput
   var newTile = $('#userInput').val();
+  // push user input to names array
   names.push(newTile);
+  // set userInput field to blank
   $('#userInput').val("");
+  // create new tile with userInput
   $(".tileContainer").append("<span class='tile'>" + newTile + "</span>");
 }
 
 // adds new tile after user presses enter or clicks + button
 $('document').ready(function(){
+    // runs addTile function on addButton click
     $('#addButton').click(function(){
         addTile();
     });
+    // runs addTile function on enter key press
     $('#userInput').keypress(function(e){
       if(e.which == 13){
         addTile();
@@ -73,7 +86,7 @@ $('document').ready(function(){
     });
 });
 
-// when clear button is clicked, clear tiles
+// when clear button is clicked, clear tiles and names array
 clearButton.addEventListener("click", function(){
   clearTiles();
   clearArray();
